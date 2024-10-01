2618d45a81d4f905299f3eeae02a765f95a66872
Sébastien <seb.juillet987@hotmail.fr>
2024-10-01 10:52:38 +0200

    change form connexion and add loader

--- componenents/protectedRoute.tsx
+++ componenents/protectedRoute.tsx
@@ -1,4 +1,31 @@
-"use client"
+// "use client"
+
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+
+// const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+//   const router = useRouter();
+//   const { data: session, status } = useSession();
+
+//   useEffect(() => {
+//     if (status === "loading") return;
+
+//     if (!session) {
+//       router.push("/");
+//     }
+//   }, [status, session, router]);
+
+//   if (status === "loading" || !session) {
+//     return null; // Ou un loader si vous le souhaitez
+//   }
+
+//   return <>{children}</>;
+// };
+
+// export default ProtectedRoute;
+
+"use client";
 
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -11,12 +38,15 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) =
   useEffect(() => {
     if (status === "loading") return;
 
-    if (!session) {
-      router.push("/");
+    // Vérifier si le token Firebase est présent dans la session
+    const firebaseToken = session?.user?.firebaseToken;
+
+    if (!firebaseToken) {
+      router.push("/"); // Rediriger si l'utilisateur n'est pas authentifié via Firebase
     }
   }, [status, session, router]);
 
-  if (status === "loading" || !session) {
+  if (status === "loading" || !session?.user?.firebaseToken) {
     return null; // Ou un loader si vous le souhaitez
   }
 
